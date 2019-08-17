9d71ad0f66f737d1685b4ffd875f798fce2e9fe0
Matt Croxson <mattcroxson@gmail.com>
2019-08-17 14:22:15 +1000

    Made register function accept optionals

--- Source/Extensions/Reusable/UITableView+Reusable.swift
+++ Source/Extensions/Reusable/UITableView+Reusable.swift
@@ -12,13 +12,17 @@ public extension UITableView {
 
     /// Registers a `Reusable` view with the table view.
     ///
-    /// - Parameter reusable: View to be registered.
-    func register(_ reusable: Reusable.Type) {
-        self.register(reusable.nib, forCellReuseIdentifier: reusable.reuseIdentifier)
+    /// - Parameter reusableType: View type to be registered.
+    func register(_ reusableType: Reusable.Type?) {
+        guard let reusableType = reusableType else { return }
+        self.register(reusableType.nib,
+                      forCellReuseIdentifier: reusableType.reuseIdentifier)
     }
 
-    func register(_ reusable: [Reusable.Type]) {
-        reusable.forEach { reusableType in
+    /// Registers an array of `Reusable` views with the table view.
+    /// - Parameter reusableTypes: Array of view types to be registered.
+    func register(_ reusableTypes: [Reusable.Type]?) {
+        reusableTypes?.forEach { reusableType in
             self.register(reusableType)
         }
     }
@@ -29,7 +33,8 @@ public extension UITableView {
     ///   - reusable: `Reusable` type to dequeue.
     ///   - indexPath: Index path of the cell being dequeued.
     /// - Returns: `Reusable` cell, or nil if the dequeue process fails.
-    func dequeueReusableCell(_ reusable: Reusable.Type, for indexPath: IndexPath) -> UITableViewCell {
+    func dequeueReusableCell(_ reusable: Reusable.Type,
+                             for indexPath: IndexPath) -> UITableViewCell {
         let cell = self.dequeueReusableCell(withIdentifier: reusable.reuseIdentifier, for: indexPath)
         return cell
     }
